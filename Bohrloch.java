7c142f216ea51307469df9bb5f8b3c8a0ea94831
Mehmet Harun Düzel <hrndzl2004@gmail.com>
2022-05-15 18:39:54 +0200

    proje portfolyomun yüzkarasi haline geldi.

--- Bohrloch.java
+++ Bohrloch.java
@@ -1,12 +1,16 @@
 public class Bohrloch {
     private int x, y;
-    private String name;
+    private String name, next;
     private double[] entfernung = new double[10];
+    private boolean besucht, istNaechste;
     
     public Bohrloch(String name, int x, int y) {
         this.name = name;
+        this.next = null;
         this.x = x;
         this.y = y;
+        this.besucht = false;
+        this.istNaechste = false;
     }
     
     public Bohrloch() {
@@ -25,23 +29,63 @@ public class Bohrloch {
         this.name = name;
     }
     
+    public String getName() {
+        return this.name;
+    }
+    
     public int getX() {
         return this.x;
+    }
+    
+    public double getXDouble() {
+        return Double.valueOf(this.x);
     }
     
     public int getY() {
         return this.y;
     }
     
-    public String getName() {
-        return this.name;
+    public double getYDouble() {
+        return Double.valueOf(this.y);
     }
     
     public double getEntfernung(int i) {
         return this.entfernung[i];
     }
     
-    public void setEntfernung(int i, int value) {
+    public void setEntfernung(int i, double value) {
         this.entfernung[i] = value;
+    }
+    
+    public double[] entfernungArray() {
+        return this.entfernung;
+    }
+    
+    public void setBesucht(boolean b) {
+        this.besucht = b;
+    }
+    
+    public boolean isBesucht() {
+        return this.besucht;
+    }
+    
+    public void setNext(String next) {
+        this.next = next;
+    }
+    
+    public String getNext() {
+        return this.next;
+    }
+    
+    // public void bestimmeNext() {
+        
+    // }
+    
+    public boolean isNext() {
+        return this.istNaechste;
+    }
+    
+    public void setNext(boolean b) {
+        this.istNaechste = b;
     }
 }
--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 
 public class Main {
     private static Bohrloch[] bohrloecher = new Bohrloch[10];
+
     public static void initializeBohrloecher() {
         for(int i = 0; i < bohrloecher.length; i++) {
             bohrloecher[i] = new Bohrloch();
         }
     }
-    
+
     public static void readFromCSV() throws FileNotFoundException {
         Scanner sc = new Scanner(new File("koordinaten.csv"));
         int i = 0;
@@ -21,28 +22,79 @@ public class Main {
             i++;
         }
     }
-    
+
     public static double eukledischeMetrik(Bohrloch bohrloch1, Bohrloch bohrloch2) {
-        double xsquare = Math.pow(bohrloch2.getX() - bohrloch1.getX(), 2);
-        double ysquare = Math.pow(bohrloch2.getY() - bohrloch1.getY(), 2);
-        double result = Math.sqrt(xsquare + ysquare);
-        return result;
+        return Math.sqrt(Math.pow(bohrloch2.getX() - bohrloch1.getX(), 2) + Math.pow(bohrloch2.getY() - bohrloch1.getY(), 2));
     }
-    
+
     public static double maximumsMetrik(Bohrloch bohrloch1, Bohrloch bohrloch2) {
         return Math.max(Math.abs(bohrloch2.getY() - bohrloch1.getY()), Math.abs(bohrloch2.getX() - bohrloch1.getX()));
     }
-    
+
     public static double manhattanMetrik(Bohrloch bohrloch1, Bohrloch bohrloch2) {
         return Math.abs(bohrloch2.getX() - bohrloch1.getX()) + Math.abs(bohrloch2.getY() - bohrloch1.getY());
+    }
+
+    public static void entfernungenZuweisen() {
+        for (int i = 0; i < bohrloecher.length; i++) {
+            for (int j = 0; j < bohrloecher[i].entfernungArray().length; j++) {
+                if (j < bohrloecher[i].entfernungArray().length - 1) {
+                    bohrloecher[i].setEntfernung(j, eukledischeMetrik(bohrloecher[j], bohrloecher[j+1]));
+                } else if (j < bohrloecher[i].entfernungArray().length) {
+                    bohrloecher[i].setEntfernung(j, eukledischeMetrik(bohrloecher[j], bohrloecher[0]));
+                }
+            }
+        }
+    }
+
+    public static void route(int i) {
+        double kleinsteEntfernung = eukledischeMetrik(bohrloecher[i], bohrloecher[i+1]);
+        int indexKleinsteEntfernung = 0;
+        for (int j = 0; j < bohrloecher[0].entfernungArray().length; j++) {
+            if (eukledischeMetrik(bohrloecher[i], bohrloecher[j]) < kleinsteEntfernung && !bohrloecher[i].isBesucht()) {
+                indexKleinsteEntfernung = j + 1;
+            }
+        }
+        bohrloecher[i].setNext(bohrloecher[indexKleinsteEntfernung].getName());
+        bohrloecher[i].setBesucht(true);
+        for (int j = 0; j < bohrloecher.length; j++) {
+            if (bohrloecher[j].getName().equals(bohrloecher[indexKleinsteEntfernung].getName())) {
+                bohrloecher[j].setNext(true);
+                bohrloecher[j].setBesucht(true);
+                bohrloecher[j].setNext(false);
+                route(j);
+                break;
+            }
+        }
+        
+        for (int j = 0; j < bohrloecher.length; j++) {
+            if (bohrloecher[j].isNext()) {
+                bohrloecher[j].setNext(false);
+                bohrloecher[j].setBesucht(true);
+                route(j);
+                break;
+            }
+        }
+    }
+
+    public static void bestimmeNext(Bohrloch[] arr, int index) {
+        double kleinsteEntfernung = eukledischeMetrik(arr[index], arr[0]);
+        int indexKleinsteEntfernung = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (eukledischeMetrik(arr[index], arr[i]) < kleinsteEntfernung) {
+                kleinsteEntfernung = eukledischeMetrik(arr[index], arr[i]);
+                indexKleinsteEntfernung = i;
+                arr[index].setNext(arr[i].getName());
+                break;
+            }
+        }
     }
     
     public static void main(String[] args) throws FileNotFoundException {
         initializeBohrloecher();
         readFromCSV();
-        
-        for (int i = 0; i < bohrloecher.length; i++) {
-            
-        }
+        bestimmeNext(bohrloecher, 0);
+        entfernungenZuweisen();
+        route(0);
     }
 }
